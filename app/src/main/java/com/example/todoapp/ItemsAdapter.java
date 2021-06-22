ad31ebbddad016b18872d8f251ab91119dd2e2f0
Pablo Blanco <pabloblanco@fb.com>
2021-06-22 17:36:45 -0500

    Created ItemsAdapter class for RecycledView
    The Adapter is the part in charge of handling the data collection and bind it to the view. It needs a respective ViewHolder to handle the data.
    
    ItemsAdapter created, with respective LayoutManager (helps position items) and ViewHolder class.
    
    List popullated with dummy data. Binding works.

---
+++ app/src/main/java/com/example/todoapp/ItemsAdapter.java
@@ -0,0 +1,70 @@
+package com.example.todoapp;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+// Class created for the adapter.
+// It is responsible for taking the data an putting it into the view holder
+public class ItemsAdapter extends RecyclerView.Adapter<ItemsAdapter.ViewHolder> {
+
+    // Class attribute for the items of the list
+    List<String> items;
+
+    // Constructor for the class
+    public ItemsAdapter(List<String> items) {
+        this.items = items;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        // Use layout inflater to inflate a view and not create a View manually
+        View todoView = LayoutInflater.from(parent.getContext()).inflate(android.R.layout.simple_list_item_1, parent, false);
+
+        // Wrap it inside a ViewHolder and return it
+        // (Create a new ViewHolder object that will be displayed)
+        return new ViewHolder(todoView);
+    }
+
+    // Responsible to binding data to a particular ViewHolder
+    // Updates a ViewHolder item by using it's bind method to update the value
+    @Override
+    public void onBindViewHolder(@NonNull ItemsAdapter.ViewHolder holder, int position) {
+        // Grab item at a specific position
+        // (Get the string at position X of the list)
+        String item = items.get(position);
+
+        // Bind the item into the specific ViewHolder
+        // (Set item's value at position X to string retrieved in last step)
+        holder.bind(item);
+    }
+
+    // Returns the length of the list
+    @Override
+    public int getItemCount() {
+        return this.items.size();
+    }
+
+    // Container that provides easy access to views that represent each row of the list
+    class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView tvItem;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            this.tvItem = itemView.findViewById(android.R.id.text1);
+        }
+
+        // Update the view inside of the ViewHolder
+        public void bind(String item) {
+            this.tvItem.setText(item);
+        }
+    }
+}
--- app/src/main/java/com/example/todoapp/MainActivity.java
+++ app/src/main/java/com/example/todoapp/MainActivity.java
@@ -1,14 +1,50 @@
 package com.example.todoapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.EditText;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Define the list of the items to-do (strings)
+    List<String> items;
+
+    // Define references from view page
+    FloatingActionButton addButton;
+    RecyclerView rvItems;
+    EditText newItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Instantiate variables from references from the views
+        addButton = findViewById(R.id.addButton);
+        newItem = findViewById(R.id.newItem);
+        rvItems = findViewById(R.id.rvItems);
+
+        // Instantiate our items list
+        items = new ArrayList<>();
+        items.add("Thing 1");
+        items.add("Thing 2");
+        items.add("Thing 3");
+
+        // Construct our adapter
+        ItemsAdapter itemsAdapter = new ItemsAdapter(this.items);
+
+        // Set our adapter in our RecyclerView
+        rvItems.setAdapter(itemsAdapter);
+
+        // Set LayoutManager (how items are displayed) -> default: vertical
+        rvItems.setLayoutManager(new LinearLayoutManager(this));
     }
 }
